c28daf2cc274e104030c1f813c002d989ceda1da
ghazaleh-19 <45766128+ghazaleh-19@users.noreply.github.com>
2018-12-24 20:26:55 +0330

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Phone {
+	public static void main(String [] args)
+	{
+		String[] name_user =new String[3];
+		String[] phone_user =new String[3];
+		for(int i=0;i<3;i++) {
+			
+			Scanner input=new Scanner(System.in);
+			System.out.println("Enter Name "+ i + " : ");
+			name_user[i]=input.nextLine();
+			System.out.println("Enter Phone " + i + " : ");
+			phone_user[i]=input.nextLine();
+			
+		}
+		
+		System.out.println("Enter name for search her phone");
+		int num;
+		Scanner input=new Scanner(System.in);
+		num=input.nextInt();
+		System.out.println("Name And Phone Number is : "+name_user[num]+" : "+ phone_user[num]);
+		
+		
+		
+		
+		
+		
+		/*int found=0;
+		int fndidx=-1;
+		
+		for( int i=0;i<3;i++)
+		{
+			
+			if(name==name_user[i])
+
+			{
+				//found=1;
+				//fndidx=i;
+				System.out.println("Phone user: "+ phone_user[i]);
+			}
+		
+		else
+		{
+			System.out.println("Not found");
+		}
+	}*/
+
+}
+}
